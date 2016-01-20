ac585e68202b899dd4bc6967e8857ba8d528390f
wangcui <wangc_os@sari.ac.cn>
2016-01-20 10:21:07 +0800

    use subdir for dojo demo

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var dojo = require('./routes/dojo');
 
 var app = express();
 
@@ -25,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/dojo', dojo);
+//app.use('/dojo', require('./routes/djdm/slide/dojo'));
+app.use('/djdm/slide/dojo', require('./routes/djdm/slide/dojo'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/djdm/slide/dojo.js
+++ routes/djdm/slide/dojo.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('demo', { title: 'Express' });
+  res.render('djdm/slide/demo', { title: 'Express' });
+  //res.send('djdm/slide/demo');
+  //res.render('demo', { title: 'Express' });
 });
 
 module.exports = router;
